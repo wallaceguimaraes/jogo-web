ddff70af600c65eea9d969df7a08fb8f6a94ef19
wallaceguimaraes <wallaceguimaraes7@outlook.com>
2018-06-29 11:50:15 -0300

    Correção

--- tabela.php
+++ tabela.php
@@ -1,15 +1,9 @@
 <?php 
-
-
-
-
 					include("Conec.php");
 
 					$consulta = "SELECT * FROM usuario ORDER BY pontuacao DESC, acerto DESC, erro ASC "; //LIMIT 20
 					$con = $mysqli->query($consulta) or die($mysqli->error);
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -26,7 +20,6 @@ function pagina(){
 <meta charset="utf-8"/>
 <title>Recordes</title>
 <link rel="stylesheet" type="text/css" href="visual/css/style.css"> 
-
 
 <style>
 
@@ -63,62 +56,49 @@ a:hover{
 animation: fadeIn 1s ease-in-out;
 }
 
-
-
 </style> 
-
-
 
 </head>
 
 <body class="fadeEntra" style="background: green;"id="corpo" >
 
-	
 	<div id="table" style="position: absolute; top: 0px; left: 200px; background: black;">	
-
-			<table id="tabela"class="tabelageral"border="2" style="padding:0;" 
- align="center">
-
+			<table id="tabela"class="tabelageral"border="2" style="padding:0;" align="center">
 					<caption id="titulo" style="color: #00FF00;"><b>Ranking</b></caption>
+						<tr>
+							<td width="300px" height="30px" style="text-align: center; border:3; color: yellow; font-size: 150%;"><b>Jogador</b></td>
+							<td width="150px" style="text-align: center; color:yellow; font-size:150%;"><b>Pontuação</b></td>
+							<td width="100px" style="text-align: center; color: yellow; font-size:150%;"><b>Acertos</b></td>
+							<td width="100px" style="text-align: center;color: yellow; font-size: 150%;"><b>Erros</b></td>
+							<td width="100px" style="text-align: center; color: yellow; font-size: 150%;"><b>Porcentagem</b></td>
+							<td width="150px" style="text-align: center; color: yellow; font-size: 150%;"><b>Dificuldade</b></td>
 
-					<tr>
-						<td width="300px" height="30px" style="text-align: center; border:3; color: yellow; font-size: 150%;"><b>Jogador</b></td>
-						<td width="150px" style="text-align: center; color:yellow; font-size:150%;"><b>Pontuação</b></td>
-						<td width="100px" style="text-align: center; color: yellow; font-size:150%;"><b>Acertos</b></td>
-						<td width="100px" style="text-align: center;color: yellow; font-size: 150%;"><b>Erros</b></td>
-						<td width="100px" style="text-align: center; color: yellow; font-size: 150%;"><b>Porcentagem</b></td>
-						<td width="150px" style="text-align: center; color: yellow; font-size: 150%;"><b>Dificuldade</b></td>
-
-					</tr>
+						</tr>
 
 					<?php 
 
+						$count = 0;
+						$st="°";
 
-					$count = 0;
-					$st="°";
+								while ($dado = $con->fetch_array()){ ?>
+								
+								<tr>
+								
+									<td style="text-align: left; color: #7FFFD4;"><?php $count++; echo "$count$st   "; echo $dado["nome_Usuario"]; ?></td>
+									<td style="text-align: center; color: #00FA9A; font-size: 120%;"><b><?php echo $dado["pontuacao"]; ?></b></td>
+									<td style="text-align: center; color: #00FF00;font-size: 120%;"><b><?php echo $dado["acerto"]; ?></b></td>
+									<td style="text-align: center;color: #FF0000;font-size: 120%;"><b><?php echo $dado["erro"]; ?></b></td>
+									<td style="text-align: center;color: #FF4500;font-size: 120%;"><b><?php echo $dado["porcento"]; ?></b></td>	
+									<td style="text-align: center;color: #00BFFF;font-size: 120%;"><b><?php echo $dado["dificuldade"]; ?></b></td>	
 
-					while ($dado = $con->fetch_array()){ ?>
-					
-					<tr>
-					
-						<td style="text-align: left; color: #7FFFD4;"><?php $count++; echo "$count$st   "; echo $dado["nome_Usuario"]; ?></td>
-						<td style="text-align: center; color: #00FA9A; font-size: 120%;"><b><?php echo $dado["pontuacao"]; ?></b></td>
-						<td style="text-align: center; color: #00FF00;font-size: 120%;"><b><?php echo $dado["acerto"]; ?></b></td>
-						<td style="text-align: center;color: #FF0000;font-size: 120%;"><b><?php echo $dado["erro"]; ?></b></td>
-						<td style="text-align: center;color: #FF4500;font-size: 120%;"><b><?php echo $dado["porcento"]; ?></b></td>	
-						<td style="text-align: center;color: #00BFFF;font-size: 120%;"><b><?php echo $dado["dificuldade"]; ?></b></td>	
-
-					</tr>
+								</tr>
 					<?php  } 
 			 ?>
 			</table>
 		
-</div>
+		</div>
+				<a id="pagina" style="position: absolute; left: 20px; top: 20px;" onclick="pagina()">Página inicial</a>
+					<script src="jquery-3.3.1.min.js"></script>
 
-<a id="pagina" style="position: absolute; left: 20px; top: 20px;" onclick="pagina()">Página inicial</a>
-
-<script src="jquery-3.3.1.min.js"></script>
-
-
-</body>
+	</body>
 </html>
--- usuario/dados/Conexao.php
+++ usuario/dados/Conexao.php
@@ -1,8 +1,7 @@
 <?php
-//Verificação de Segurança precisa ser feita aqui
 
 class Conexao {
-     //Estes dados devem ser coletados de outro lugar... claro...
+
      private static $host = "localhost";
      private static $user = "id5811126_wallace";
      private static $password = "wallace7";
@@ -11,11 +10,9 @@ class Conexao {
      private static $conexao;
 
 
-    //Esta função retorna uma conexão ativa, ou cria uma conexão nova e retorna a mesma.
      public static function getConnection() {
         if (Conexao::$conexao == null) {
             try {
-                //connect as appropriate as above
                 Conexao::$conexao = new PDO("mysql:host=".Conexao::$host . ";dbname=" . Conexao::$database . ";charset=" . Conexao::$charset, Conexao::$user, Conexao::$password);
                 return Conexao::$conexao;
             } catch (PDOException $ex) {
